5c79d9d356034c20e9e3417970b27fb8bfc201f8
NadaSamir13 <nadasamirsodan@gmail.com>
2025-12-01 07:21:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,46 @@
-#include "bits
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+template <typename T, int order>
+class B_Tree
+{
+private:
+
+    struct Node
+    {
+        T keys[order - 1];
+        Node* child[order];
+        int keyCount1;
+        bool is_leaf;
+
+        Node(bool leaf)
+        {
+            is_leaf= leaf;
+            keyCount1 = 0;
+            for(int p = 0; p < order; p++)
+            {
+                child[p] = NULL;
+            }
+        }
+    };
+
+    Node* ro;
+
+public:
+
+    B_Tree()
+    {
+        ro = NULL;
+    }
+
+    /*void Insert(T value);
+    void Print();
+    void InsertNonFull(Node* node, T value);
+    void SplitChild(Node* parent, int index);
+    void Print(Node* node, int level);*/
+};
+
+
+int main () {
+    return 0;
+}
